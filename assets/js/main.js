3d32ecf7d7f8fa1a38729c7b9b27253eb743d846
Ana Julia Neiverth <115558556+anajvn@users.noreply.github.com>
2023-09-20 17:29:35 -0300

    Add page and cards backgound color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,37 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMore')
 
+const limit = 5;
+let offset = 0;
 
-function convertPokemonTypesToLi(pokemonTypes){
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)   
-}
-
-function convertPokemonToLi(pokemon){
-    return `
-        <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+function loadPokemonItens(offset, limit){
+    pokeApi.getPokemons(offset, limit)
+    .then((pokemons) => {
+        const listItems = pokemons.map( pokemon => `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.sprites.other.dream_world.front_default}"
+                <img src="${pokemon.photo}"
                 alt=${pokemon.name}>
             </div>
         </li>
-    `
+    `).join('')
+        pokemonList.innerHTML += listItems
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList')
-            
-pokeApi.getPokemons()
-    .then((pokemons) => {
-        const listItems = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML = listItems
-    })
-    .catch((error) => console.log(error))
+// Runs as the program opens
+loadPokemonItens(offset, limit)
 
+// Runs when clicked
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
     
